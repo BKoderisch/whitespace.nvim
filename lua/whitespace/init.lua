98a1a07a64bca0d661ae58c28af3f825052cc72e
Benjamin Koderisch <b.koderisch+github@gmail.com>
2025-05-01 17:49:54 +0200

    Add Plugin code

---
+++ lua/whitespace/init.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+local group = vim.api.nvim_create_augroup("WhiteSpaceGroup", { clear = true })
+local enabled = true
+
+function M.toggle_trailing_whitespace()
+  enabled = not enabled
+  if enabled then
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = group,
+      pattern = "*",
+      callback = function()
+        vim.api.nvim_set_hl(0, "TrailingWhitespace", { bg = "DarkRed" })
+        vim.cmd("match TrailingWhitespace /\\s\\+$/")
+      end,
+    })
+  else
+    vim.api.nvim_clear_autocmds({ group = group })
+    vim.cmd("match none")
+  end
+end
+
+function M.remove_trailing_whitespace()
+  local current_view = vim.fn.winsaveview()
+  vim.cmd([[ %s/\s\+$//e ]])
+  vim.fn.winrestview(current_view)
+end
+
+return M
