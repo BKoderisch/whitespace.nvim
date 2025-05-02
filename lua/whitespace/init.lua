0b58e02838828f51d2f53936e1a1cf7cfd0a5c93
Benjamin Koderisch <b.koderisch+github@gmail.com>
2025-05-02 17:35:34 +0200

    Add config

--- lua/whitespace/init.lua
+++ lua/whitespace/init.lua
@@ -1,11 +1,20 @@
 local M = {}
 
 local group = vim.api.nvim_create_augroup("WhiteSpaceGroup", { clear = true })
-local enabled = true
+local enabled = false
+local config = {
+  color = "DarkRed",
+  default_state = true,
+}
 
-function M.toggle_whitespace_highlight()
-  enabled = not enabled
-  vim.api.nvim_set_hl(0, "TrailingWhitespace", { bg = "DarkRed" })
+function M.setup(opts)
+  config = vim.tbl_deep_extend("force", config, opts or {})
+  M.set_whitespace_highlight(config.default_state)
+end
+
+function M.set_whitespace_highlight(state)
+  enabled = state
+  vim.api.nvim_set_hl(0, "TrailingWhitespace", { bg = config.color })
   if enabled then
     vim.api.nvim_create_autocmd("BufEnter", {
       group = group,
@@ -18,6 +27,10 @@ function M.toggle_whitespace_highlight()
   else
     vim.cmd("match none")
   end
+end
+
+function M.toggle_whitespace_highlight()
+  M.set_whitespace_highlight(not enabled)
 end
 
 function M.remove_trailing_whitespace()
